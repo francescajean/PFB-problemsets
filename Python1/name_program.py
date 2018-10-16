ada228bf88f323e32ab914a8dc47850c7c976240
fje <fje@pfb18.cshl.edu>
2018-10-16 11:24:39 -0400

    updated name code to run with sys

--- Python1/name_program.py
+++ Python1/name_program.py
@@ -1,1 +1,12 @@
-print('My name = Francesca')
+#!/usr/bin/env python3
+import sys
+
+names = sys.argv[1]
+colour = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print('My name = ', names)
+print('My favourite colour = ', colour)
+print('My favourite activity = ' , activity)
+print('My favourite animal = ' , animal)

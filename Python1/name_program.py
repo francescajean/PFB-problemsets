ec4b57d3b5f22f9578eec2278347489398d3d79a
fje <fje@pfb18.cshl.edu>
2018-10-16 11:57:49 -0400

    added a new line, bell sound, and done at the end

--- Python1/name_program.py
+++ Python1/name_program.py
@@ -6,7 +6,5 @@ colour = sys.argv[2]
 activity = sys.argv[3]
 animal = sys.argv[4]
 
-print('My name = ', names)
-print('My favourite colour = ', colour)
-print('My favourite activity = ' , activity)
-print('My favourite animal = ' , animal)
+print('My name = ', names, '\nMy favourite colour = ', colour, '\nMy favourite activity = ', activity, '\nMy favourite animal = ', animal)
+print('Done! \a\n\n\n')

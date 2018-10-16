0ffc3e34f6a05909b0bb1e686165ab9fc767e168
fje <fje@pfb18.cshl.edu>
2018-10-16 15:44:56 -0400

    making my first if_else statement and it works

---
+++ Python2/if_else_statement.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+variable = sys.argv[1]
+
+if variable == 0:
+  message = "True"
+  print (variable,message)
+
+else:
+  message = "Not True"
+  print (variable,message)

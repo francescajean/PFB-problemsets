5b27e179856bb2da19546340618aaf1a279b5e58
fje <fje@pfb18.cshl.edu>
2018-10-16 16:59:15 -0400

    completed the alternate code for the python 2 lesson

--- Python2/alternate_pos_or_neg.py
+++ Python2/alternate_pos_or_neg.py
@@ -7,8 +7,12 @@ if number > 0:
   print('Postive')
   if number < 50:
     print('Smaller than 50')
-    if number%2==0:
+    if (number%2)==0:
       print('It is an even number that is smaller than 50')
+  elif number > 50:
+    print('Larger than 50')
+    if (number%3)==0:
+      print('It is larger than 50 and divisible by 3')
 elif number == 0:
   print('Equal to zero')
 else:

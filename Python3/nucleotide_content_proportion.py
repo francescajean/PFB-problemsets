9a30dce5c5cd9a1ec7c4cca25fcf272c1f95ae70
fje <fje@pfb18.cshl.edu>
2018-10-17 13:50:50 -0400

    added comments to each of the scripts to make it clear what each step was

--- Python3/nucleotide_content_proportion.py
+++ Python3/nucleotide_content_proportion.py
@@ -4,18 +4,18 @@ DNA = 'GATGGGATTGGGGTTTTCCCCTCCCATGTGCTCAAGACTGGCGCTAAAAGTTTTGAGCTTCTCAAAAGTCTAG
 
 #DNA = 'AATTCCGG'
 
-DNA.count('A')
+DNA.count('A')#counting the number of As
 As = DNA.count('A')
 
-DNA.count('T')
+DNA.count('T')#counting the number of Ts
 Ts = DNA.count('T')
 
-As_and_Ts = As + Ts
+As_and_Ts = As + Ts#the total number of As and Ts in the sequence
 
-len(DNA)
+len(DNA)#calculating the length of the total DNA sequence
 TotalDNA = len(DNA)
 
-print(As_and_Ts/TotalDNA,'%',sep='')
+print(As_and_Ts/TotalDNA,'%',sep='')#dividing the number of As/Ts by the total number of DNA, putting this in a percent, and then taking out the separator using the sep= function); finally printing the answer to the screen
 
 
 
